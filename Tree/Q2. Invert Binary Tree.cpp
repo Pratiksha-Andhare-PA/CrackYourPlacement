a8a47154d7b1536a23e3cc2f0c019980461a540a
Pratiksha Andhare <128924098+Pratiksha-Andhare-PA@users.noreply.github.com>
2024-11-20 08:01:41 +0530

    Solution uploaded

---
+++ Tree/Q2. Invert Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return NULL;
+
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
+
+/**
+
+TC => O(N)
+SC => O(h)
+
+**/

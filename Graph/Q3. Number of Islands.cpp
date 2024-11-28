d096c80a0db38c783cd60af2bea4c5a909721791
Pratiksha Andhare <128924098+Pratiksha-Andhare-PA@users.noreply.github.com>
2024-11-28 16:25:38 +0530

    Solution uploaded

---
+++ Graph/Q3. Number of Islands.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    void dfs(vector<vector<char>> &grid, int i, int j, int m, int n){
+        // Base case: Check if we are out of bounds or if the cell is water ('0')
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+
+        // Mark the cell as visited by setting it to '0'
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j, m, n);
+        dfs(grid, i + 1, j, m, n);
+        dfs(grid, i, j - 1, m, n);
+        dfs(grid, i, j + 1, m, n);
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int cnt = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                // If the cell is land ('1'), start a DFS
+                if(grid[i][j] == '1'){
+                    cnt++;                   // Found a new island
+                    dfs(grid, i, j, m, n);   // Mark the entire island as visited
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+/**
+
+# Complexity:
+TC => O(M*N)
+SC => O(M*N)
+
+**/

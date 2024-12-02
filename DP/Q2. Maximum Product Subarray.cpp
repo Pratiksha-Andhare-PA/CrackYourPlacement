a6cfdc40e4db550d91904f2b97ed0a5214b9d905
Pratiksha Andhare <128924098+Pratiksha-Andhare-PA@users.noreply.github.com>
2024-12-02 22:03:59 +0530

    Solution uploaded

--- DP/Q2. Maximum Product Subarray.cpp
+++ DP/Q2. Maximum Product Subarray.cpp
@@ -2,25 +2,43 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int cp=1;
 
-        int mx1 = INT_MIN;
-        int mx2 = INT_MIN;
+        int ans = INT_MIN;
+        int pre = 1, suff = 1;
 
-        for(int i=0;i<n;i++){
-            cp *= nums[i];
-            mx1 = max(mx1,cp);
-            if(cp == 0)
-                cp = 1;
+        for(int i = 0; i < n; i++){
+            if(pre == 0) pre = 1;
+            if(suff == 0) suff = 1;
+
+            pre *= nums[i];
+            suff *= nums[n - i - 1];
+
+            ans = max(ans, max(pre, suff));
         }
-        
-        cp = 1;
-        for(int i=n-1;i>=0;i--){
-            cp *= nums[i];
-            mx2 = max(mx2,cp);
-            if(cp == 0)
-                cp = 1;
-        }
-        return max(mx1,mx2);
+
+        return ans;
     }
 };
+
+/**
+
+# Approaches:
+
+1) Brute Force: 2 Nested loops
+TC => O(N^2)
+SC => O(1)
+
+2) Optimal: 
+3 cases: a) All +ve no.
+         b) Even -ve no.
+         c) Odd -ve no.
+
+    In case a & b, the ans will be the product of the entire array
+    In case of c, remove 1 -ve no. such that will get the maximum product.
+    To handle this, use prefix and suffix products.
+    Also, handle the case of having num 0. Reset pre/suff to 1 accordingly.
+
+TC => O(N)
+SC => O(1)
+
+**/
